9cff9e5f376ff21ec2c60a719c7450ac8c8d72b5
vduyy <57698328+vduyyy@users.noreply.github.com>
2020-07-29 08:35:50 -0700

    Add files via upload

---
+++ destructuring.js
@@ -0,0 +1,51 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+// Tesla, Mercedes 
+console.log(randomCar)
+console.log(otherRandomCar)
+
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: name } = employee;
+// its break because the name not defined 
+console.log(name);
+// console.log(otherName);
+// copy
+
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+// password: 12345
+console.log(password);
+console.log(hashedPassword);
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+// False && True 
+console.log(first == second);
+console.log(first == third);
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
